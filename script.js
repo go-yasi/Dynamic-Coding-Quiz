5c027e2b14f8feac28bc04c9266bac03a511c28c
Yasi Moshtael <yasi.moshtael@gmail.com>
2021-05-19 18:49:50 -0700

    Added what I think would be the beginning step of adding an event listener to the start button so the timer would start upon click. Then thought to start creating a timer function

---
+++ script.js
@@ -0,0 +1,21 @@
+var startButton = document.querySelector("#start-button");
+
+
+
+
+
+
+// Create a function for the timer to start
+function timer() {
+
+};
+
+
+
+
+
+
+// Start timer on click of startButton
+startButton.addEventListener("click", function(){
+
+});
