1d16a42125641aa9d1a704143a9acb8215854253
Yasi Moshtael <yasi.moshtael@gmail.com>
2021-05-19 19:35:59 -0700

    Timer function wasn't working, so I wrapped the subtract one from time left in an if statement, and added text to the timer elemet to show the current count. Added else statements for when timer reaches zero

--- script.js
+++ script.js
@@ -1,19 +1,24 @@
 var startButton = document.querySelector("#start-button");
+var timer = document.querySelector("#timer");
 
 
 
 
 
-
-// Create a function for the timer to start
+// Create a function for the timer
 function startTimer() {
-    var = timeLeft = 30;
+    var timeLeft = 5;
 
     var timeInterval = setInterval(function () {
-        timeLeft--;
-
-        if(timeLeft === 0) {
+        if (timeLeft > 1) {
+            timeLeft--;
+            timer.textContent = "Time: " + timeLeft;
+        }
+        
+        else {
+            timer.textContent = "";
             clearInterval(timeInterval);
+            window.alert("Time is up!");
         }
 
     }, 1000)
