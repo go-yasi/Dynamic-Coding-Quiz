110db68d39f0b15f170a556b5859c3f53d780b66
Yasi Moshtael <yasi.moshtael@gmail.com>
2021-05-20 17:14:34 -0700

    Made a lot of edist and updates to the JavaSript, unfortunately without any concrete idea of where I was going with it. Mostly testing different avenues to see what would and wouldn't work. Most of it did not work :(

--- script.js
+++ script.js
@@ -1,6 +1,42 @@
 var startButton = document.querySelector("#start-button");
 var timer = document.querySelector("#timer");
 
+var quizDiv = document.querySelector("#quiz");
+
+// Object with questions and answer options
+var questions = [{
+    questions: "Here is the question?",
+    answers: ["A1", "A2", "A3", "A4"],
+    correct: "A3"
+},
+{
+    questions: "Now another is the question?",
+    answers: ["A1", "A2", "A3", "A4"],
+    correct: "A3"
+},
+{
+    questions: ["Here is the question?"],
+    answers: ["A1", "A2", "A3", "A4"],
+    correct: "A3"
+},
+{
+    questions: ["Here is the question?"],
+    answers: ["A1", "A2", "A3", "A4"],
+    correct: "A3"
+}];
+
+
+// var questions = [{
+//     questions: "Question?",
+//     answers: ["1", "2", "3," "4"],
+//     correct: "xxx"
+// },
+// {
+//     questions: "Question?",
+//     answers: ["Boolean", "x"],
+//     correct: "xxx"
+// }];
+console.log(questions);
 
 // Function to start timer
 function startTimer() {
@@ -12,10 +48,31 @@ function startTimer() {
             timeLeft--;
             timer.textContent = "Time: " + timeLeft + " seconds";
         } else {
-            timer.textContent = "";
+            timer.textContent = "Game Over!";
             clearInterval(timeInterval);
         }
     }, 1000)
+};
+
+// defining questionCounter start value
+var questionCounter = 0;
+console.log(questionCounter);
+
+// Function for first question 
+function firstQuestion(){
+    var question1 = questions[questionCounter];
+    console.log(question1);
+
+    var question = question1.question;
+
+    var question2 = document.createElement("h2");
+    question2.textContent = question;
+    quizDiv.appendChild();
+
+    var answers = question1.answers;
+    var correct = question1.correct;
+
+    questionCounter++;
 };
 
 // Start timer on click of startButton
